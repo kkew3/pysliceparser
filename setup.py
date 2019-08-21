1a593be1f0fb744e5c7e7c3bc35efa73ab17b12a
kkew3 <kps6326@hotmail.com>
2019-08-21 13:55:26 +0800

    Correctly attached README to PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+_cd = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(_cd, 'README.md')) as infile:
+    long_description = infile.read()
+
 setup(
     name='sliceparser',
     packages=['sliceparser'],
-    version='0.9b',
+    version='0.9.1b',
     license='MIT',
     description='Parse numpy style advanced indexing notation from string.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Kaiwen Wu',
     author_email='kps6326@hotmail.com',
     url='https://github.com/kkew3/pysliceparser',
